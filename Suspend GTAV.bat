ce087333a18459cb6b739a77177033c4e644f474
gyfen <59654516+gyfen@users.noreply.github.com>
2020-01-13 22:17:22 +0100

    Update Suspend GTAV.bat

--- Suspend GTAV.bat
+++ Suspend GTAV.bat
@@ -1,5 +1,12 @@
 @ECHO OFF
+title Grand Theft Auto V Suspender
+tasklist /FI "IMAGENAME eq GTA5.exe" 2>NUL | find /I /N "GTA5.exe">NUL
+if "%ERRORLEVEL%"=="0" (
 start pssuspend64.exe GTA5.exe
 echo Succesfully suspended process.
 timeout /t 10 /nobreak
 start pssuspend64.exe -r GTA5.exe
+) else (
+Echo Grand Theft Auto V is not running.
+pause
+)
